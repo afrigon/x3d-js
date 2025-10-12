70aa3247dbc3ade38cad05d619cbb9110fe452a6
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2025-10-12 15:26:33 -0400

    actually fixed broken build

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "eslint/config"
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
-import react from "eslint-plugin-react"
-import reactHooks from "eslint-plugin-react-hooks"
 import prettier from "eslint-config-prettier/flat"
 
 export default defineConfig(

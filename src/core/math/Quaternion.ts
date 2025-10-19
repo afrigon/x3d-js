ba012ae539d57c37b4b3dd7dd8bee38fc45f3f05
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2025-10-19 05:37:36 -0400

    fixed quaternion multiplication

--- src/core/math/Quaternion.ts
+++ src/core/math/Quaternion.ts
@@ -27,10 +27,10 @@ export class Quaternion {
 
     static multiply(a: Quaternion, b: Quaternion, ...rest: Quaternion[]): Quaternion {
         const result = new Quaternion(
-            a.x * b.x,
-            a.y * b.y,
-            a.z * b.z,
-            a.w * b.w,
+            a.w * b.x + a.x * b.w + a.y * b.z - a.z * b.y,
+            a.w * b.y - a.x * b.z + a.y * b.w + a.z * b.x,
+            a.w * b.z + a.x * b.y - a.y * b.x + a.z * b.w,
+            a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z
         )
 
         const next = rest[0]

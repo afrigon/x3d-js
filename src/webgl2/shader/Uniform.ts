48f40441f81d188a2b91e8310ceb85022caa94c4
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2025-10-18 23:28:45 -0400

    array of structs and array fix

--- src/webgl2/shader/Uniform.ts
+++ src/webgl2/shader/Uniform.ts
@@ -7,7 +7,7 @@ export type UniformPrimitive =
     | { type: "vec4" }
     | { type: "mat4" }
 
-export type UniformArray = { type: "array", of: UniformPrimitive, size: number }
+export type UniformArray = { type: "array", of: Uniform, size: number }
 export type UniformStruct = { type: "struct", name: string }
 export type Uniform = UniformPrimitive | UniformArray | UniformStruct
 
